dea7404f70cf296ac65a77427d8f9c15d6cad94c
Yosua Tarida Panjaitan <lomylix@gmail.com>
2023-12-11 08:04:45 +0700

    fix: failed to lookup view directory

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const MemoryStore = require("memorystore")(session);
 const flash = require("express-flash");
 const morgan = require("morgan");
 const router = require("./routes");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -24,6 +25,7 @@ app.use(
 );
 app.use(flash());
 app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, '../views'));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(router);
