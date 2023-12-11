a8d86f5327bd56dcb50739380ca0fc99e2069d97
Yosua Tarida Panjaitan <lomylix@gmail.com>
2023-12-11 08:07:45 +0700

    fix: failed to lookup public directory

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const MemoryStore = require("memorystore")(session);
 const flash = require("express-flash");
 const morgan = require("morgan");
 const router = require("./routes");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -25,9 +25,9 @@ app.use(
 );
 app.use(flash());
 app.use(express.urlencoded({ extended: true }));
-app.set('views', path.join(__dirname, '../views'));
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(router);
 
 app.listen(PORT, console.log(`Snapr running on port ${PORT}`));
